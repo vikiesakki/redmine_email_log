636313fc54abbf78a8d1bf2dd0e7a45d4d57216f
Vignesh <vemcse@gmail.com>
2025-09-02 18:52:16 +0530

    Bug fixes on the redmine log

--- lib/redmine_hook.rb
+++ lib/redmine_hook.rb
@@ -1,12 +1,12 @@
 class RedmineHook  < Redmine::Hook::ViewListener
 
     def view_layouts_base_html_head(context = {})
-        stylesheet_link_tag('that', :plugin => 'that_email_log') +
-        javascript_include_tag('that', :plugin => 'that_email_log')
+        stylesheet_link_tag('that', :plugin => 'redmine_email_log') +
+        javascript_include_tag('that', :plugin => 'redmine_email_log')
     end
 
     def view_layouts_base_content(context = {})
-        if User.current.admin? && Setting.plugin_that_email_log['contextual_email_logs'] && context[:request].format == 'text/html'
+        if User.current.admin? && Setting.plugin_redmine_email_log['contextual_email_logs'] && context[:request].format == 'text/html'
             if context[:controller].controller_name == 'issues' && context[:controller].action_name == 'show'
                 context[:controller].send(:render_to_string, :partial => 'email_logs/issues')
             elsif context[:controller].controller_name == 'messages' && context[:controller].action_name == 'show'
